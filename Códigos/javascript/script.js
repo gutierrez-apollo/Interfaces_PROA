ecf3be6badc341ba5b3bc237b530aad11439b7f4
Ramon Gutierrez <89090825+gutierrez-apollo@users.noreply.github.com>
2021-09-29 20:41:00 -0300

    Delete Códigos/javascript directory

--- Códigos/javascript/script.js
+++
@@ -1,8 +0,0 @@
-function jonny() {
-    document.getElementById('paragrafo').innerHTML = Date();
-}
-
-function mudar() {
-    document.getElementById('paragrafo').style.fontSize = "20px";
-    document.getElementById('paragrafo').style.backgroundColor = "blue";
-}
